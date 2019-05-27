9b78c6926c3b1c70dcc97f4d9ae274ef808cba65
chaeyeon <codus09191@likelion.org>
2019-05-27 19:20:48 +0900

    완료

--- model/settings.py
+++ model/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8$7^_&$@00b_9(#y7(xmgv$#7r52^4eq)ic^a9(-pus0d#7kny'
-
+# SECRET_KEY = '8$7^_&$@00b_9(#y7(xmgv$#7r52^4eq)ic^a9(-pus0d#7kny'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
